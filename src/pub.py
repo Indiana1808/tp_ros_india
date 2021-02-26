3d3215d40a7661de501eefae5719203c2350a85b
Indiana1808 <jenitatindiana@yahoo.com>
2021-02-26 22:44:40 +0400

    tp1

--- src/pub.py
+++ src/pub.py
@@ -1,67 +1,8 @@
 #!/usr/bin/env python
-"""import rospy
-from geometry_msgs.msg import PoseStamped
-import math 
-class publisher:
-	def __init__(self):
-		rospy.init_node('publisher', anonymous = True)
-		self.pub = rospy.Publisher('publish_topic',PoseStamped, queue_size=15)
-		self.rate = rospy.Rate(20)
-
-	def run(self):
-		message=PoseStamped()
-		rate=rospy.Rate(15)
-		x=0,0
-		y=0,0
-		vx=-1,0
-		vy=-1,0
-		message.header.frame_id="map"
-		while not rospy.is_shutdown():
-			theta = theta+dt
-			message.pose.position.x=math.cos(theta)
-			message.pose.position.y=math.sin(theta)
-			self.pub.publish(message)
-			self.rate.sleep()
-
-if __name__ == '__main__':
-	try:
-	    node=publisher()
-	    node.run()
-	except rospy.ROSInterruptException:
-	    pass """
-
-		 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""#!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import PoseStamped
-import math 
+import math
+import turtle
 class publisher:
 	def __init__(self):
 		rospy.init_node('publisher', anonymous = True)
@@ -71,13 +12,23 @@ class publisher:
 	def run(self):
 		message=PoseStamped()
 		rate=rospy.Rate(20)
-		theta=0
-		dt=0.1
+		tur= turtle.Turtle()
 		message.header.frame_id="map"
 		while not rospy.is_shutdown():
-			theta = theta+dt
-			message.pose.position.x=math.cos(theta)
-			message.pose.position.y=math.sin(theta)
+			for i in range(4): 
+				tur.forward(100) 
+				tur.left(90) 
+			
+			
+			"""message.pose.position.x=tur.forward(100) 
+			message.pose.position.y=tur.left(90)
+			message.pose.position.z=tur.forward(100)
+			message.pose.position.e=tur.left(90)
+			message.pose.position.a=tur.forward(100)
+			message.pose.position.c=tur.left(90)
+			message.pose.position.f=tur.forward(100)
+			message.pose.position.e=tur.left(90)"""
+			
 			self.pub.publish(message)
 			self.rate.sleep()
 
@@ -86,7 +37,7 @@ if __name__ == '__main__':
 	    node=publisher()
 	    node.run()
 	except rospy.ROSInterruptException:
-	    pass """
+	    pass 
 
 		 
 			 
